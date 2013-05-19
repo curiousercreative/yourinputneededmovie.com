2d2436dc19ab00ac658690efaf097bed4f27638a
Winston Hoy <winston@curiousercreative.com>
2013-05-19 15:17:27 -0400

    - working on scaling function, let's try something new

--- js/yin.js
+++ js/yin.js
@@ -17,6 +17,45 @@ winston@curiousercreative.com
 
 // Execute when document structure is ready
     $(document).ready(function () {            
-    // Hide the body until loaded        
+    // Website scaling
+        scaleWebsite = function () {
+            var w = $(window).width()*0.95625;
+            var h = $(window).height();
+            
+            if (w/(h-35-$('#footer').height()) > 2125/1460) {
+            // It won't fit, manually set the width and let it center
+                var newWidth = (h-35-$('#footer').height())*2125/1460+'px';
+            }
+            else {
+            // It'll fit, scale up
+                var newWidth = '95.625%';
+            }
+            
+        // Set the width of the body
+            $('body').css('width', newWidth);
+            
+        // Set the width and margin of the footer
+            $('#footer').width(newWidth);
+            $('#footer').css({
+                width: newWidth,
+                left: ($(window).width()-$('body').width())/2+'px'
+            });
+    
+        // Set the appropriate font-size
+            fontSize = $('body').width() * 14/1530;
+            $('body').css('font-size', fontSize+'px');
+        }
         
+    // Scale video
+        scaleVideo = function () {
+            $('#video').height($('#video').width()*9/16);   
+        }
+        
+        $(window).resize(function () {
+           scaleWebsite();
+           scaleVideo();
+        });
+        
+        scaleWebsite();
+        scaleVideo();
     });
